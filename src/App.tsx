8ee99266238ceccb7fdecbd88ec3cc848b71c4c8
Omar Salloum <omarsalloum08@gmail.com>
2025-12-14 01:30:21 +0100

    small changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
 import { Routes, Route } from "react-router-dom";
-import GuestRoute from "./components/Auth/GuestRoute.tsx";
-import ProtectedRoute from "./components/Auth/ProtectedRoute.tsx";
-import OwnerRoute from "./components/Auth/OwnerRoute.tsx";
-import EmployeeRoute from "./components/Auth/EmployeeRoute.tsx";
+import GuestRoute from "./components/Auth/GuestRoute";
+import ProtectedRoute from "./components/Auth/ProtectedRoute";
+import OwnerRoute from "./components/Auth/OwnerRoute";
+import EmployeeRoute from "./components/Auth/EmployeeRoute";
 
-import WelcomePage from "./components/WelcomePage/WelcomePage.tsx";
-import LoginPage from "./components/Auth/LoginPage/LoginPage.tsx";
-import CompanyRegistrationPage from "./components/CompaniesPage/CompaniesPage.tsx";
-import OwnerDashboardPage from "./components/OwnerDashboard/OwnerDashboardPage.tsx";
-import EmployeeDashboardPage from "./components/EmployeeDashboardPage/EmployeeDashboardPage.tsx";
-import NotFoundPage from "./components/NotFoundPage/NotFoundPage.tsx";
+import WelcomePage from "./components/WelcomePage/WelcomePage";
+import LoginPage from "./components/Auth/LoginPage/LoginPage";
+import CompanyRegistrationPage from "./components/CompaniesPage/CompanyRegistrationPage/CompanyRegistrationPage";
+import OwnerDashboardPage from "./components/OwnerDashboard/OwnerDashboardPage";
+import EmployeeDashboardPage from "./components/EmployeeDashboardPage/EmployeeDashboardPage";
+import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
     <Routes>
-      {/* Guest-only routes */}
       <Route element={<GuestRoute />}>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register-company" element={<CompanyRegistrationPage />} />
       </Route>
 
-      {/* Authenticated routes */}
       <Route element={<ProtectedRoute />}>
         <Route element={<OwnerRoute />}>
           <Route path="/owner-dashboard" element={<OwnerDashboardPage />} />
@@ -35,7 +33,6 @@ function App() {
         </Route>
       </Route>
 
-      {/* 404 */}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
